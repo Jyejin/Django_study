0052269ca8b332f0e401d11d37cb19a8f5e01fa8
yejin <dbswjd1977@gmail.com>
2018-10-28 21:22:08 +0900

    ch5-done

--- ch5/polls/urls.py
+++ ch5/polls/urls.py
@@ -2,8 +2,8 @@ from django.urls import re_path
 from polls import views
 
 urlpatterns = [
-    re_path(r'^$', views.index, name='index'),
-    re_path(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+    re_path(r'^$', views.IndexView.as_view(), name='index'),
+    re_path(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
     re_path(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
-    re_path(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
+    re_path(r'^(?P<pk>\d+)/results/$', views.ResultView.as_view(), name='results'),
 ]
--- ch5/polls/views.py
+++ ch5/polls/views.py
@@ -3,21 +3,40 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from polls.models import Question, Choice
+from django.views.generic import ListView, DetailView
+import logging
+logger = logging.getLogger(__name__)
 
-def index(request):
-    latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+class IndexView(ListView):
+    template_name = "polls/index.html"
+    context_object_name = 'latest_question_list'
 
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
-def detail(request, question_id):
-    #pk = question_id 검색조건에 맞는 객체가 없으면 404에러
-    #detail.html 텍스트 데이터를 담은 HttpResponse 객체를 반환
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question':question})
+class DetailView(DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultView(DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+# def index(request):
+#     latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'polls/index.html', context)
+#
+#
+# def detail(request, question_id):
+#     #pk = question_id 검색조건에 맞는 객체가 없으면 404에러
+#     #detail.html 텍스트 데이터를 담은 HttpResponse 객체를 반환
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question':question})
 
 
 def vote(request, question_id):
+    logger.debug("vote().question_id: %s" %question_id)
     p = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = p.choice_set.get(pk=request.POST['choice'])
@@ -35,3 +54,6 @@ def vote(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question':question})
+
+
+
